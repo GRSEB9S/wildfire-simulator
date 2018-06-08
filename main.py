2c3e466e84ae861c9991bc032d9612252b62f0ed
Liam Nickell <liam.nickell25@gmail.com>
2018-06-08 14:49:30 -0500

    reworked the way tkinter is used to create gui

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-import application
+import Tkinter as tk
+
+root = tk.Tk()
+kRows = 50
+kCols = 50
+buttonGrid = []
+
+def setupGUI():
+    root.title('Wildfire Simulator')
+    generateGrid(kRows, kCols)
+    
+
+
+def generateGrid(numRows, numCols):
+    for r in range(0, numRows): 
+        buttonGrid.append([])
+        for c in range(0, numCols):
+            button = tk.Button(root, command=lambda row=r, col=c: toggleCell(row, col), bg='green')
+            button.grid(row=r, column=c)
+            buttonGrid[r].append(button)
+
+
+def toggleCell(row, col):
+    pass
+
 
 def main():
-    app = application.GUI()
-    app.master.title('Wildfire Simulator')
-    app.mainloop()
+    pass
 
 
 if __name__ == '__main__':
